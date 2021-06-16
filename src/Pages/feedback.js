47dadbfe39a0ea56c979bf8a179b600a47fab856
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2021-06-16 15:40:00 -0300

    criei page feedback

---
+++ src/Pages/feedback.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Header from '../components/Header';
+
+class Feedback extends React.Component {
+  render() {
+    return <Header />;
+  }
+}
+
+export default Feedback;
