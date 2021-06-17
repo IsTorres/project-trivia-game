da0f0e80b40db55a1c26edcd7846e9272503694b
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-06-17 15:39:47 -0300

    18 feita

--- src/Pages/ranking.js
+++ src/Pages/ranking.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 export default class ranking extends Component {
+  constructor() {
+    super();
+    this.state = {
+      home: false,
+    };
+    this.redirectHome = this.redirectHome.bind(this);
+  }
+
+  redirectHome() {
+    this.setState({ home: true });
+  }
+
   render() {
+    const { home } = this.state;
+    if (home) {
+      return <Redirect to="/" />;
+    }
     return (
       <div>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.redirectHome }
+        >
+          New-Game
+
+        </button>
       </div>
     );
   }
